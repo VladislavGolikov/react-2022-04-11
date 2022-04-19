591d3a43ae9fc32cad535571664a8f151f7145ce
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-19 18:23:09 +0300

    второе задание вроде работает?

--- src/features/restaurant/ui/restaurants/component.jsx
+++ src/features/restaurant/ui/restaurants/component.jsx
@@ -1,9 +1,19 @@
+import {useState} from 'react';
+
 import { Restaurant } from "../restaurant/component";
 
 export const Restaurants = ({ restaurants }) => {
-  return (
-    <div>
-      <Restaurant restaurant={restaurants[0]} />
-    </div>
-  );
+
+    let [currentRestaurant, selectRestaurant]=useState(0);
+
+    const selectRestaurants=restaurants.map((el,ind)=>{
+        return <button  onClick={()=>selectRestaurant(currentRestaurant=ind)} key={el.id}>{el.name}</button>
+    })
+
+    return (
+        <div>
+          {selectRestaurants}
+          <Restaurant restaurant={restaurants[currentRestaurant]} />
+        </div>
+    );
 };
