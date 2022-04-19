822b09f0f7dac0d233e3b5e6ededfb6a2558bb1b
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-19 17:25:03 +0300

    первое задание есть, и сделано без циклов!

--- src/features/rate/ui/rate/component.jsx
+++ src/features/rate/ui/rate/component.jsx
@@ -1,1 +1,11 @@
-export const Rate = ({ value }) => <span>{value}</span>;
+import stars from "./star.svg";
+
+/* ох и пришлось помучаться, чтобы без циклов обойтись!! ведь eval в jsx не работает... */
+
+export const Rate = ({ value }) => {
+
+    const allStars=`<img src=${stars} alt="star" height="10px"/>`.repeat(value);
+
+    return <span dangerouslySetInnerHTML={{__html: allStars}}></span>
+
+}
