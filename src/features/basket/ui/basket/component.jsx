dd3cfcdd6ee53e62d3fd3f1eb209e5b11694346d
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-27 20:23:12 +0300

    добавлены стили в баскет

--- src/features/basket/ui/basket/component.jsx
+++ src/features/basket/ui/basket/component.jsx
@@ -2,9 +2,15 @@ import classnames from "classnames";
 import styles from "./styles.module.scss";
 
 export const Basket = ({ className }) => {
+
+    const name='здесь будет название продуктов';
+    const quantity=55;
   return (
     <div className={classnames(styles.root, className)}>
-      <span>Basket</span>
+      <span className={classnames(styles.title, className)}>Наша корзина:</span>
+      <div className={classnames(styles.name, className)}>{name}</div>
+      <div className={classnames(styles.quantity, className)}>{quantity}</div>
+      <button className={classnames(styles.drop, className)}>Очистить корзину</button>
     </div>
   );
 };
