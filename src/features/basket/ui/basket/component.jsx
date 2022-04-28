8344ec5d8085072dfb668453db0724e5b4b2382a
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2022-04-28 17:24:24 +0300

    попытка по образцу сделать контейнер для баскета - пока провалилась

--- src/features/basket/ui/basket/component.jsx
+++ src/features/basket/ui/basket/component.jsx
@@ -1,16 +1,13 @@
 import classnames from "classnames";
 import styles from "./styles.module.scss";
 
-export const Basket = ({ className }) => {
-
-    const name='здесь будет название продуктов';
-    const quantity=55;
+export const Basket = ({className, name, quantity, drop}) => {
   return (
     <div className={classnames(styles.root, className)}>
       <span className={classnames(styles.title, className)}>Наша корзина:</span>
       <div className={classnames(styles.name, className)}>{name}</div>
       <div className={classnames(styles.quantity, className)}>{quantity}</div>
-      <button className={classnames(styles.drop, className)}>Очистить корзину</button>
+      <button className={classnames(styles.drop, className)} onClick={drop}>Очистить корзину</button>
     </div>
   );
 };
